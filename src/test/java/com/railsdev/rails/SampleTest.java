4a6ef8644bb597d9b88126cf0adf147a93a66beb
Hamish Weir <55558643+signal32@users.noreply.github.com>
2021-07-19 21:19:23 +0100

    Devops: Setup Junit 5

---
+++ src/test/java/com/railsdev/rails/SampleTest.java
@@ -0,0 +1,13 @@
+package com.railsdev.rails;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void helloWorld(){
+        Assertions.assertEquals("hello world", "hello world");
+    }
+}
